6b78600d29bcefdb7a31ae47190e8835a0f1493c
akansha8114 <akanshavgk2004@gmail.com>
2025-03-04 00:37:06 +0530

    Added codes

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,18 +1,28 @@
-const ItemList = ({items}) =>{
-    return(
-        <div>
-            
-            {items.map((item)=> 
-                (<div key={item.card.info.id}>
-                    <div> 
-                        <span>{item.card.info.name}</span>
-                        <span>{item.card.info.price/100}</span>
-                    </div>
-                    <p>{item.card.info.description}</p>
+// Step 33" component for the body content of the accordian it will list the items name under each category
+import { CDN_URL } from "../utils/contents";
 
-            </div>))}
-            
+const ItemList = ({ items }) => {
+  return (
+    <div className="ItemList">
+      {items.map((item) => (
+        <div key={item.card.info.id} className="Item ItemCard">
+          <div className="ItemContent">
+            <span className="ItemName">{item.card.info.name}</span>
+            <span className="ItemPrice">
+              ₹ {item.card.info.price ? item.card.info.price / 100 : item.card.info.defaultPrice / 100}
+            </span>
+          </div>
+          <div className="ItemDescription">
+            <p>{item.card.info.description}</p>
+          </div>
+          <div className="ItemActions">
+            <button className="Addbtn">Add +</button>
+            <img className="img" src={CDN_URL + item.card.info.imageId} alt={item.card.info.name} />
+          </div>
         </div>
-    )
-}
-export default ItemList
+      ))}
+    </div>
+  );
+};
+
+export default ItemList;
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,20 +1,71 @@
 import ItemList from "./ItemList";
+import React from "/node_modules/react";
+import { useState } from "/node_modules/react";
 // Step 32 : Building the accordian for each category
-const RestaurantCategory = ({data}) => {
-    console.log("Recived data",data);
-    return(
-        <div>
-            {/* Accordian Header */}
-            <div className="Accordian-header">
-                <div >
-                    <span>{data.title} ({data.itemCards.length})</span>
-                    <span>⋁</span>
-                </div>
-                {/* Accordian Body */}
-                <ItemList items={data.itemCards}></ItemList>
+
+const RestaurantCategory = ({ data }) => {
+    console.log("Received data:", data);
+
+    // Step 34: Create a state variable and initialize it to false to hide items initially
+    const [showItems, setShowItems] = useState(false);
+
+    //Step 35 : this will handle the click whenever we will click on the header of the accordian it will toggle the items b/w shpwing and hiding
+    const handleClick = () => {
+        setShowItems(!showItems);
+    };
+
+    return (
+        <div className="accordion-container">
+           
+            {/* Accordion Header */}
+            <div className="Accordian-header" onClick={handleClick}>
+                <span className="accordion-title">
+                    {data.title} ({data.itemCards.length})
+                </span>
+                <span className = "accordion-icon">▼</span> 
             </div>
+
+            {/* Accordion Body */}
+            {showItems && (
+                <div className="Accordian-Body">
+                    <div className="accordion-content">
+                        <ItemList items={data.itemCards} />
+                    </div>
+                </div>
+            )}
+            
         </div>
-    )
-}
+    );
+};
 
 export default RestaurantCategory;
+
+// const RestaurantCategory = ({data}) => {
+//     console.log("Recived data",data);
+
+//    
+//     const [showItems, setShowItems] = useState(false);
+
+//     
+//     const handleClick = () => {
+//         setShowItems(!showItems);
+//     }
+//     return(
+//         <div>
+//             {/* Accordian Header */}
+//             <div className="Accordian-header">
+//                 <div onClick={handleClick} >
+//                     <span>{data.title} ({data.itemCards.length})</span>
+//                     <span>⋁</span>
+//                 </div>
+//                 {/* Accordian Body */}
+//                 <div className="Accordian-Body">
+//                    {showItems &&<ItemList items={data.itemCards}></ItemList>}
+//                 </div>
+                
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default RestaurantCategory;
